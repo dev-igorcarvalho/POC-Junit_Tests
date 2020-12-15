851639a0adb8e218a1bdf653ed3b21adf37f63f4
igor carvalho <igor.carvalho@datagrupo.com.br>
2020-12-15 13:49:37 -0300

    incluindo tags para filtrar os testes que serao executados ou nao

---
+++ src/test/java/br/com/igorcarvalho/tests/JunitTaggedTests.java
@@ -0,0 +1,45 @@
+package br.com.igorcarvalho.tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * @Tag() marca uma classe ou metodo com uma tag
+ * e posteriormente podemos usar o nome da tag para filtrar
+ * condicionalmente a execução dos testes usando o maven.
+ * Classes ou metodos podem conter 1 ou mais @tag()
+ * Nomes da tag nao podem ser nulos ou em branco,
+ * nao podem conter espaços e nao podem conter
+ * caracteres reservados ",()&|!" etc
+ * Para filtrar os tests pelas tags usamos no surfire
+ * plugin do maven as tags <includeTags>nome-da-tag</includeTags>
+ * e <excludeTags>nome-da-tag</excludeTags>
+ * as
+ * */
+@Tag("nome-da-tag")
+@SpringBootTest
+class JunitTaggedTests {
+
+
+    @Tag("nome1")
+    @Tag("tag-extra")
+    @Test
+    void teste1() {
+        assertTrue(true);
+    }
+    @Tag("nome2")
+    @Test
+    void teste2() {
+        assertTrue(true);
+    }
+    @Tag("nome3")
+    @Test
+    void teste3() {
+        assertTrue(true);
+    }
+
+}
