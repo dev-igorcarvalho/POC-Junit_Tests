cfb70ed572a5651f2906ef406390cac1ca13b0d1
igor carvalho <igor.carvalho@datagrupo.com.br>
2021-01-04 19:53:14 -0300

    refatorando para ter imutabilidade

--- src/main/java/br/com/igorcarvalho/tests/tdd/money/Dollar.java
+++ src/main/java/br/com/igorcarvalho/tests/tdd/money/Dollar.java
@@ -2,13 +2,13 @@ package br.com.igorcarvalho.tests.tdd.money;
 
 public class Dollar {
 
-    public double amount;
+    public final double amount;
 
     public Dollar(double value) {
         this.amount=value;
     }
 
-    public void times(double value) {
-        this.amount*=value;
+    public Dollar times(double value) {
+        return new Dollar(this.amount*value);
     }
 }
