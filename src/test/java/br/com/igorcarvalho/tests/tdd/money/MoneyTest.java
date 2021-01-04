32f4bc8c16558cb934b64bf96076d49a5ad2c943
igor carvalho <igor.carvalho@datagrupo.com.br>
2021-01-04 19:56:35 -0300

    refatorando para ter um setup() predefinido para todos os testes

--- src/test/java/br/com/igorcarvalho/tests/tdd/money/MoneyTest.java
+++ src/test/java/br/com/igorcarvalho/tests/tdd/money/MoneyTest.java
@@ -1,15 +1,22 @@
 package br.com.igorcarvalho.tests.tdd.money;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DollarTest {
 
+    Dollar fiveDollars;
+
+    @BeforeEach
+    void setUp() {
+         fiveDollars = new Dollar(5.0);
+    }
+
     @Test
     void multiplicationTest() {
-        Dollar initialValue = new Dollar(5.0);
-        Dollar finalValue = initialValue.times(2.0);
+        Dollar finalValue = fiveDollars.times(2.0);
         assertEquals(10, finalValue.getAmount());
     }
 
